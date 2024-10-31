1e878fd241b953a9edee122765873fd50a22feb1
José <jn10632@gmail.com>
2024-10-31 12:58:17 -0300

    testes unitarios em funcionamento

--- src/main/java/com/wallet/WalletApplication.java
+++ src/main/java/com/wallet/WalletApplication.java
@@ -2,9 +2,12 @@ package com.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
+@EnableJpaRepositories(basePackages = "com.repository")
+@EntityScan(basePackages = "entity")
 public class WalletApplication {
 
 	public static void main(String[] args) {
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -2,11 +2,14 @@ package entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Entity
+@Table(name = "users")
 @Data
+@NoArgsConstructor
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1693850165739564098L;
@@ -14,10 +17,13 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String password;
+
     @Column(nullable = false)
     private String name;
+
     @Column(nullable = false)
     private String email;
 }
